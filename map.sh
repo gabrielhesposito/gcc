7fb5ba805f68056c70cc60ffb3552ebeeb87750a
gabriel.h.esposito <gabriel.h.esposito@gmail.com>
2015-11-08 11:39:13 -0500

    fixed range everything else is good, range is difficult to judge at the moment

--- map.sh
+++ map.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-
-nmap $1 -n -sP | grep report | awk '{print $5}' >& active_machines.txt
+IP=$(ifconfig br0 | grep 'inet addr' | cut -d: -f2 | awk '{print $1}')
+RANGE=150-200
+echo ${IP:0:-2}$RANGE
+nmap ${IP:0:-2}$RANGE -n -sP | grep report | awk '{print $5}' >& active_machines.txt
