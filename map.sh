20e78a73630c76ef9ff6dd83192583bc876905c3
gabriel.h.esposito <gabriel.h.esposito@gmail.com>
2015-11-08 12:40:17 -0500

    using fixed value for network trim

--- map.sh
+++ map.sh
@@ -3,4 +3,4 @@ IP=$(ifconfig eth0 | grep 'inet addr' | cut -d: -f2 | awk '{print $1}')
 ##still needs to be dynamic no bother for dev
 ## find bottom of ip pool, first ip after router then sweep up
 RANGE=150-200
-nmap ${IP:0:-2}$RANGE -n -sP | grep report | awk '{print $5}' >& iplist.txt
+nmap ${IP:0:-3}$RANGE -n -sP | grep report | awk '{print $5}' >& iplist.txt
