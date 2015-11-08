1c55d5108c511e92768ecf3ad021d711dcc81eaa
gabriel.h.esposito <gabriel.h.esposito@gmail.com>
2015-11-08 12:09:53 -0500

    changed file name

--- map.sh
+++ map.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 IP=$(ifconfig br0 | grep 'inet addr' | cut -d: -f2 | awk '{print $1}')
+##still needs to be dynamic no bother for dev
+## find bottom of ip pool, first ip after router then sweep up
 RANGE=150-200
-echo ${IP:0:-2}$RANGE
-nmap ${IP:0:-2}$RANGE -n -sP | grep report | awk '{print $5}' >& active_machines.txt
+nmap ${IP:0:-2}$RANGE -n -sP | grep report | awk '{print $5}' >& iplist.txt
